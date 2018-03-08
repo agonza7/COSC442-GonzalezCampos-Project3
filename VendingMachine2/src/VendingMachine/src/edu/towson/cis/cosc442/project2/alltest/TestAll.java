aa6e2b3f7732e2ee092cf1bcb1f8ac2304021d79
Andy Gonzalez <agonza7@students.towson.edu>
2018-03-08 11:53:33 -0500

    task 2 done ...only 79.4% achieved

---
+++ VendingMachine2/src/VendingMachine/src/edu/towson/cis/cosc442/project2/alltest/TestAll.java
@@ -0,0 +1,32 @@
+package VendingMachine.src.edu.towson.cis.cosc442.project2.alltest;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The class <code>TestAll</code> builds a suite that can be used to run all
+ * of the tests within its package as well as within any subpackages of its
+ * package.
+ *
+ * @generatedBy CodePro at 3/8/18 11:22 AM
+ * @author agonc
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	VendingMachine.src.edu.towson.cis.cosc442.project2.vendingmachine.TestAll.class,
+})
+public class TestAll {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 3/8/18 11:22 AM
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { TestAll.class });
+	}
+}
